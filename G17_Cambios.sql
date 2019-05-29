d665e1dfc0182cbc333da87b46eaf44646c06723
Eloy Mazza <eloymazza.ra@gmail.com>
2019-05-28 23:37:53 -0300

    Se cambiaron todas las ocurrencias de GXX a G17

--- G17_Cambios.sql
+++ G17_Cambios.sql
@@ -1,10 +1,10 @@
 -- Control de consistencia de fechas (Fecha desde no debe ser mayor a fecha hasta)
-ALTER TABLE GRXX_ALQUILER ADD CONSTRAINT CHK_GRXX_ALQUILER_CONSISTENTE_FECHA CHECK (
+ALTER TABLE GR17_ALQUILER ADD CONSTRAINT CHK_GR17_ALQUILER_CONSISTENTE_FECHA CHECK (
     to_char(fecha_desde, 'YYYY-MM-DD') <= to_char(fecha_hasta, 'YYYY-MM-DD')
     OR fecha_desde IS NULL
 );
 
 -- Control de tipos posibles de posicion 
-ALTER TABLE GRXX_POSICION ADD CONSTRAINT CHK_GRXX_POSICION_TIPO CHECK (
+ALTER TABLE GR17_POSICION ADD CONSTRAINT CHK_GR17_POSICION_TIPO CHECK (
     tipo IN ('general', 'vidrio', 'insecticidas', 'inflamable');
 );
