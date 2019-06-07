5631d3cfa5766e7a61345f4ed177348bba623372
Eloy Mazza <eloymazza.ra@gmail.com>
2019-06-06 21:21:44 -0300

    terminado

--- db install/G17_Cambios.sql
+++ db install/G17_Cambios.sql
@@ -1,7 +1,6 @@
 -- Control de consistencia de fechas (Fecha desde no debe ser mayor a fecha hasta)
 ALTER TABLE GR17_ALQUILER ADD CONSTRAINT CHK_GR17_ALQUILER_CONSISTENTE_FECHA CHECK (
     to_char(fecha_desde, 'YYYY-MM-DD') <= to_char(fecha_hasta, 'YYYY-MM-DD')
-    OR fecha_desde IS NULL
 );
 --INSERCION PROMUEVE RESTRICCION CHK_GR17_ALQUILER_CONSISTENTE_FECHA:
 /*
